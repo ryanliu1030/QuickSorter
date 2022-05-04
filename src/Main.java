4b4d2c774ba4d6688978cf8a14e0b44adad576f0
Xinrui Liu <xinrui.liu.1030@gmail.com>
2022-05-04 03:43:02 -0500

    ver 1.0

--- src/Main.java
+++ src/Main.java
@@ -2,48 +2,40 @@ import java.time.*;
 import java.util.*;
 import java.io.*;
 public class Main {
-    public static void main(String[] args)
-    {
-        if (args.length == 4)
-        {
-            try {
-                File reportF = new File(args[1]);
-                File unsortF = new File(args[2]);
-                File sortF = new File(args[3]);
-                
-                PrintWriter report = new PrintWriter(reportF);
-                PrintWriter unsorted = new PrintWriter(unsortF);
-                PrintWriter sorted = new PrintWriter(sortF);
-                
-                int size = Integer.parseInt(args[0]);
-                
-                ArrayList<Integer> list = QuickSorter.generateRandomList(size);
-                unsorted.println(list);
-                unsorted.close();
-                ArrayList<Integer> first = new ArrayList<>(list);
-                ArrayList<Integer> random = new ArrayList<>(list);
-                ArrayList<Integer> median3 = new ArrayList<>(list);
-                ArrayList<Integer> median3Rand = new ArrayList<>(list);
-                
-                Duration firstElement, randElement, medianThree, medianRandThree;
-                firstElement = QuickSorter.timedQuickSort(first, QuickSorter.PivotStrategy.FIRST_ELEMENT);
-                randElement = QuickSorter.timedQuickSort(random, QuickSorter.PivotStrategy.RANDOM_ELEMENT);
-                medianRandThree = QuickSorter.timedQuickSort(median3, QuickSorter.PivotStrategy.MEDIAN_OF_THREE_RANDOM_ELEMENTS);
-                medianThree = QuickSorter.timedQuickSort(median3Rand, QuickSorter.PivotStrategy.MEDIAN_OF_THREE_ELEMENTS);
-                sorted.println(first);
-                sorted.close();
-                
-                report.println("Array Size = " + size);
-                report.println("FIRST_ELEMENT : " + firstElement);
-                report.println("RANDOM_ELEMENT : " + randElement);
-                report.println("MEDIAN_OF_THREE_RANDOM _ELEMENTS : " + medianRandThree);
-                report.println("MEDIAN_OF_THREE_ELEMENTS : " + medianThree);
-                report.close();
-            }
+    public static void main(String[] args) throws FileNotFoundException {
+        if (args.length == 4) {
+            File reportF = new File(args[1]);
+            File unsortF = new File(args[2]);
+            File sortF = new File(args[3]);
 
-            catch(Exception e) {
-                System.out.println("Unable to create file");
-            }
+            PrintWriter report = new PrintWriter(reportF);
+            PrintWriter unsorted = new PrintWriter(unsortF);
+            PrintWriter sorted = new PrintWriter(sortF);
+
+            int size = Integer.parseInt(args[0]);
+
+            ArrayList<Integer> list = QuickSorter.generateRandomList(size);
+            unsorted.println(list);
+            unsorted.close();
+            ArrayList<Integer> first = new ArrayList<>(list);
+            ArrayList<Integer> random = new ArrayList<>(list);
+            ArrayList<Integer> median3 = new ArrayList<>(list);
+            ArrayList<Integer> median3Rand = new ArrayList<>(list);
+
+            Duration firstElement, randElement, medianThree, medianRandThree;
+            firstElement = QuickSorter.timedQuickSort(first, QuickSorter.PivotStrategy.FIRST_ELEMENT);
+            randElement = QuickSorter.timedQuickSort(random, QuickSorter.PivotStrategy.RANDOM_ELEMENT);
+            medianRandThree = QuickSorter.timedQuickSort(median3, QuickSorter.PivotStrategy.MEDIAN_OF_THREE_RANDOM_ELEMENTS);
+            medianThree = QuickSorter.timedQuickSort(median3Rand, QuickSorter.PivotStrategy.MEDIAN_OF_THREE_ELEMENTS);
+            sorted.println(first);
+            sorted.close();
+
+            report.println("Array Size = " + size);
+            report.println("FIRST_ELEMENT : " + firstElement);
+            report.println("RANDOM_ELEMENT : " + randElement);
+            report.println("MEDIAN_OF_THREE_RANDOM _ELEMENTS : " + medianRandThree);
+            report.println("MEDIAN_OF_THREE_ELEMENTS : " + medianThree);
+            report.close();
         }
         else
             System.out.println("Please Input Valid Arguments in Command Lines");
