22f641af5daef38fe95545f852718272e21b1544
Xinrui Liu <xinrui.liu.1030@gmail.com>
2022-05-04 03:40:08 -0500

    ver 1.0

--- src/QuickSorter.java
+++ src/QuickSorter.java
@@ -5,35 +5,37 @@ public class QuickSorter {
     private QuickSorter() {}
 
     //assign enums that's needed
-    public static enum PivotStrategy{
-        FIRST_ELEMENT, RANDOM_ELEMENT, MEDIAN_OF_TWO_ELEMENTS, MEDIAN_OF_THREE_ELEMENTS
+    public enum PivotStrategy{
+        FIRST_ELEMENT, RANDOM_ELEMENT, MEDIAN_OF_THREE_RANDOM_ELEMENTS, MEDIAN_OF_THREE_ELEMENTS
     }
 
     public static <E extends Comparable<E>> Duration timedQuickSort(ArrayList<E> list, PivotStrategy pivotStrategy) throws NullPointerException{
-        if (list.isEmpty() || list == null)
+        if (list.isEmpty())
             throw new NullPointerException("Argument Invalid.");
         long startTime = System.nanoTime();
-        PivotStrategy selection = pivotStrategy;
-        switch (selection) {
-            case FIRST_ELEMENT -> {
-                firstElementHelper(list, 0, list.size() - 1);'
-            }
+        switch (pivotStrategy) {
+            case FIRST_ELEMENT ->
+                firstElementHelper(list, 0, list.size() - 1);
 
-            case RANDOM_ELEMENT -> {
-                randomElementHelper(list, 0, list.size() -1);
-            }
-            case MEDIAN_OF_TWO_ELEMENTS -> {
-                threeMedianRandomHelper(list, 0, list.size() -1);
-            }
-            case MEDIAN_OF_THREE_ELEMENTS -> {
-                threeMedianHelper(list, 0, list.size() - 1);
-            }
+            case RANDOM_ELEMENT -> randomElementHelper(list, 0, list.size() -1);
+            case MEDIAN_OF_THREE_RANDOM_ELEMENTS -> threeMedianRandomHelper(list, 0, list.size() -1);
+            case MEDIAN_OF_THREE_ELEMENTS -> threeMedianHelper(list, 0, list.size() - 1);
         }
 
         long endTime = System.nanoTime();
         return Duration.ofNanos(endTime - startTime);
     }
 
+
+    public static ArrayList<Integer> generateRandomList(int size){
+        ArrayList<Integer> list = new ArrayList<>(size);
+        Random rand = new Random();
+        for (int i = 0; i < size; i ++){
+            int temp = rand.nextInt();
+            list.add(temp);
+        }
+        return list;
+    }
     private static <E extends Comparable<E>> void swap(ArrayList<E> list, int i, int j){
         E temp = list.get(i);
         E temp2 = list.get(j);
@@ -74,11 +76,30 @@ public class QuickSorter {
         randomElementHelper(list, pivot + 1, high);
     }
 
+
+    private static <E extends Comparable<E>> int median(ArrayList<E> list, int a, int b, int c){
+        if (list.get(a).compareTo(list.get(b)) > 0) {
+            if (list.get(b).compareTo(list.get(c)) > 0)
+                return b;
+            else if (list.get(a).compareTo(list.get(c)) > 0)
+                return c;
+            else
+                return a;
+        }
+        else {
+            if (list.get(a).compareTo(list.get(c)) > 0)
+                return a;
+            else if (list.get(b).compareTo(list.get(c)) > 0)
+                return c;
+            else return b;
+        }
+
+    }
     private static <E extends Comparable<E>> void threeMedianHelper(ArrayList<E> list, int low, int high){
         if (low >= high)
             return;
 
-        int medianPivot = getMedian(list, low, high, (low+high) / 2);
+        int medianPivot = median(list, low, high, (low+high) / 2);
         if(medianPivot != high)
             swap(list, medianPivot, high);
 
